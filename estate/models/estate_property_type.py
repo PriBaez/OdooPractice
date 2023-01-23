81778842550e0164dcd07d949219ef8368321b89
Prisila E. Baez Mendez <prisi.e.baez@gmail.com>
2023-01-22 21:55:21 -0400

    Done chapter 11: Inline Views

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,7 @@ class EstatePropertyType(models.Model):
     _description = "Type/Sort of property\'s on sale"
 
     name = fields.Char(string='Property\'s Type', required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id")
 
     _sql_constraints = [('estate_type_unique', 'UNIQUE (name)',
                         'The name must be unique')
